3e251b12d694ae32c5b409a89aa3aeb2fc8c36db
yousef issa <yousef@u.northwestern.edu>
2017-06-14 00:35:15 -0500

    Change api.

--- tracker.py
+++ tracker.py
@@ -1,11 +1,11 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
 import tweepy
+import json
 
 # url to scrape from. don't abuse my key or ill make you enter your own :/
-wci_url = 'https://www.worldcoinindex.com/apiservice/json?key=qSt7Iu0d312Ro5yqmB3m5NuZ1'
 
-# get the web content
-soup = BeautifulSoup(requests.get(wci_url).text, "html.parser")
+# get the json content of cryptos
+btc = json.loads(requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR').text)
 
-print(soup)
+
