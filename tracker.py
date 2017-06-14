e8ebbccc8948c799705eb9c56383811fc16ecd0c
yousef issa <yousef@u.northwestern.edu>
2017-06-14 01:13:26 -0500

    removed prints.

--- tracker.py
+++ tracker.py
@@ -21,20 +21,18 @@ def monitor():
 	old_btc_price, old_eth_price = get_prices()
 	new_btc_price, new_eth_price = old_btc_price, old_eth_price
 	# look for the price differnece
-	print(abs((old_btc_price['USD']/new_btc_price['USD']) - 1))
-	print(abs((old_eth_price['USD']/new_eth_price['USD']) -1))
 	while abs((old_btc_price['USD']/new_btc_price['USD']) - 1) < .05 and abs((old_eth_price['USD']/new_eth_price['USD']) -1) < .05:
 		sleep(60)
 		new_btc_price, new_eth_price = get_prices()
 	# send tweet when it breaks out, then restart.
-	# api.update_status('Change in price! \n\nBTC: {} USD, {} EUR\nETH: {} USD, {} EUR'.format(new_btc_price['USD'], new_btc_price['EUR'], new_eth_price['USD'], new_eth_price['EUR']))
+	api.update_status('Change in price! \n\nBTC: {} USD, {} EUR\nETH: {} USD, {} EUR'.format(new_btc_price['USD'], new_btc_price['EUR'], new_eth_price['USD'], new_eth_price['EUR']))
 	print('Price change tweeted!')
 	monitor()
 
 # tweet price initially
 def first_tweet():
 	btc_price, eth_price = get_prices()
-	# api.update_status('Prices! \n\nBTC: {} USD, {} EUR\nETH: {} USD, {} EUR'.format(btc_price['USD'], btc_price['EUR'], eth_price['USD'], eth_price['EUR']))
+	api.update_status('Prices! \n\nBTC: {} USD, {} EUR\nETH: {} USD, {} EUR'.format(btc_price['USD'], btc_price['EUR'], eth_price['USD'], eth_price['EUR']))
 	print('Initial price tweeted!')
 
 if __name__ == "__main__":
