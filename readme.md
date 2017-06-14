fa2fbd6c321b81e84963323351f496f95fd8fafb
yousef issa <yousef@u.northwestern.edu>
2017-06-14 00:56:29 -0500

    Updates

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 Tweets the coin value if the change is over a certain percent.
 
 # ToDo:
+	Tweet prices every 90 minutes unless there is a 5% change.
 	Monitor change from single exchange
 	Monitor change from various exchanges
 	Tweet changes out to twitter.
--- tracker.py
+++ tracker.py
@@ -1,11 +1,27 @@
 from requests import get
-from bs4 import BeautifulSoup
 import tweepy
-import json
+from json import loads
+import time
 
-# url to scrape from. don't abuse my key or ill make you enter your own :/
+btc = ('BTC', 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR')
+eth = ('ETH', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR')
 
-# get the json content of cryptos
-btc = json.loads(requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR').text)
+# gets prices from api
+def get_prices():
+	return loads(get(btc[1]).text), loads(get(eth[1]).text)
+
+# monitors for a 5% change or greater every minute
+def monitor():
+	old_btc_price, old_eth_price = get_prices()
+	new_btc_price, new_eth_price = old_btc_price, old_eth_price
+	# look for the price differnece
+	while abs((old_btc_price/new_eth_price) - 1) < .05 and abs((old_eth_price/new_eth_price) -1) < .05:
+		sleep(60)
+		new_btc_price, new_eth_price = get_prices()
+	# send tweet when it breaks out, then restart.
+	tweet_api.update_status('Change in price! \n\nBTC: {} USD, {}\nETH: {},{}'.format(new_btc_price['USD'], new_btc_price['EUR'], new_eth_price['USD'], new_eth_price['EUR']))
+	monitor()
 
 
+if __name__ == "__main__":
+	monitor()
